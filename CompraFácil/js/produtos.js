f56f17ba198bb8b05dc301e23feed2cc5bca36d2
Doelísio Gaspar <doegaspar123@gmail.com>
2026-03-13 12:08:02 +0100

    mm

--- CompraFácil/js/produtos.js
+++ CompraFácil/js/produtos.js
@@ -48,7 +48,15 @@ const bancadas = [
                 categoria: "salgado",
                 tag: "🍕 ITALIANA"
             },
-            
+            {
+                id: 103,
+                nome: "Biscoito",
+                descricao: "Biscoito fresquinho",
+                preco: 50,
+                imagem: "Biscoito.jpeg",
+                categoria: "salgado",
+                tag: "🥟 Suave"
+            },
             {
                 id: 104,
                 nome: "Risóis",
